af8259c6c0f075a9a8ffbab93c297903c7e1da96
Tugrul Erdem <tugrulerdemdogru@gmail.com>
2021-07-15 14:58:16 +0300

    added: clean localstorage

--- udemy/movie/ui.js
+++ udemy/movie/ui.js
@@ -55,13 +55,24 @@ UI.prototype.loadAllFilms = function(films){
     const filmList = document.getElementById("films");
     films.forEach(function (film){
         filmList.innerHTML += `
-        <tr>
+
+    <tr>
     <td><img src="${film.url}" class="img-fluid img-thumbnail" style="width: 30%;"></td>
     <td>${film.title}</td>
     <td>${film.director}</td>
     <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
     </tr> 
-        
+    
         `;
     });
+}
+
+
+UI.prototype.deleteFilmFromUI = function(element) {
+    element.parentElement.parentElement.remove();
+
+    
+
+
+
 }
